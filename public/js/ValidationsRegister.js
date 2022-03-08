1a9cf5701fd9246172158b1438863aa0b02be43c
Andress10 <andres_20nuz@hotmail.com>
2022-03-08 02:26:11 -0300

    Andres, subi algunos cambios y realize validaciones js Front

--- public/js/ValidationsRegister.js
+++ public/js/ValidationsRegister.js
@@ -1,18 +1,51 @@
-window.onload = function(){
-
+window.addEventListener('load', function(){
 
 let form = document.querySelector('.form');
 let fullName = document.querySelector('#fullName');
 let email = document.querySelector('#email');
 let password = document.querySelector('#password');
 let confirmPassword = document.querySelector('#confirm');
+let buton = document.querySelector('.buton');
 
 
- fullName.addEventListener("focus",function(){console.log("1")})
+    form.addEventListener("submit", function(event){
+    
+    
 
+        let errores = [];
 
-form.addEventListener('submit',function(e){
-    e.preventDefault()
+        if(fullName.value == ''){
+            errores.push('el campo de nombre no puede estar vacio')
+        }else if( fullName.value.length < 3){
+            errores.push("el campo de nombre debe tener al menos 3 caracteres")
+        }
+    
+
+        if(email.value =="" ){
+            errores.push('el campo de email no puede estar vacio') }
+        
+        if(password.value == "") {
+            errores.push('debes completar password')
+        }
+        else if(password.value >= 8){
+            errores.push('el password debe contener al menos 8 caracteres')
+        }
+
+        
+
+        if (errores.length > 0){
+            event.preventDefault();
+
+            let UlError = document.querySelector("div.errores ul")
+            for (let i = 0; i < errores.length; i++) {
+                
+                UlError.innerHTML += "<li>" + errores[i] + "</li>"
+            }
+        }
+    })
 })
 
-}
+
+
+
+
