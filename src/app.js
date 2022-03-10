08e03af86e97599d06a8014daff25418c1f8f0e3
Mayra Gutierrez <gutierrez.charcas97@gmail.com>
2022-03-10 13:48:43 -0300

    Mayra G. - Agregando vista de errores

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 /* EXPRESS*/
+var createError = require('http-errors');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -36,6 +37,22 @@ app.set('views', path.resolve(__dirname, 'views'));
 // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	next(createError(404));
+  });
+
+  // error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+  });
 
 /* REQUERIMIENTO DE RUTAS */
 const mainRutas = require('./routers/indexRouter');
