5093564a570b40bd1b5fe08a927feb3859b5aee5
Marcelo Olivera <olivera.m.et13@gmail.com>
2022-03-14 21:01:14 -0300

    Marcelo Olivera - Terminando menu responsivo

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -2,13 +2,13 @@ let db = require("../database/models");
 
 const controllerIndex = {
     home: (req,res) =>{
-        db.Category.findAll({
-            where: {
-                isSpecial: 1
-            }
-        })
-        .then(homeProducts => {
-            res.render('./menu/index', {homeProducts});
+        db.Product.findAll({where:{category_id:4},limit:4})
+        .then( producto => {
+            db.Category.findAll({where:{isSpecial:1}})
+                .then( categories => {
+                    res.render('./menu/index', {producto, categories})
+                })
+                .catch(error => res.send(error))
         })
     },
 
